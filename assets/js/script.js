f9870a03a9a3dc396476bd4edd87dafe3864e2c5
Norboro <ajbrook19@gmail.com>
2023-02-09 20:16:46 -0500

    Start experimenting with foundation css.

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const token = 'f56350323de774e7aaf1d605e1cb07985f350a92'
+$(document).foundation();
+//var imageEl = $("#image")
+
+/*fetch("https://wger.de/api/v2/exercise/?muscles=1")
+.then(function(result){
+    return result.json()
+}).then(function(data){
+    console.log(data.results);
+    fetch("https://wger.de/api/v2/exerciseinfo/"+data.results[0].id)
+    .then(function(result){
+        return result.json()
+    }).then(function(data){
+        console.log(data);
+        imageEl.attr('src', data.images[0].image);
+    });
+});*/
+
